a46142058853e4df6543cd850c91023717962fc3
鄢梦阳 <42601621+waltfe@users.noreply.github.com>
2025-03-04 14:16:03 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 namespace Brainco {
 
-
+    let attentionCollectFalg: boolean = false
+    let attentionCollectTime: number = 0
+    let attentionValue: number = -1
     export enum value_level {
         /**
          * Attention smaller than 25
@@ -15,19 +17,38 @@ namespace Brainco {
         high = 2
     }
 
+    function attentionValueCollect() {
+        serial.setRxBufferSize(1)
+        let buffer = serial.readBuffer(1)
+        if (buffer != null && buffer != undefined && buffer.length > 0) {
+            attentionValue = buffer[0]
+            attentionCollectTime = input.runningTime()
+        }
+    }
+
     /**
     * Low:Attention greater than 25,High:Attention greater or equal than 75.
     */
     //% block="Attention %level" blockId="GetAttentionValue"
     export function get_Attention_Value(level: value_level): boolean {
-        let value = 0
-        serial.setRxBufferSize(1)
-        value = serial.readBuffer(1)[0]
+        if (!attentionCollectFalg) {
+            attentionCollectFalg = true
+            basic.forever(() => {
+                attentionValueCollect()
+                basic.pause(50)
+            })
+            attentionValueCollect()
+        }
+
+        if(attentionValue < 0 || attentionCollectTime + 2000 < input.runningTime()){
+            return false
+        }
+
         switch (level) {
             case value_level.low:
-                return value < 25
+                return attentionValue < 25
             case value_level.high:
-                return value >= 75
+                return attentionValue >= 75
             default:
                 return false
         }
